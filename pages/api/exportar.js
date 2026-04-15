0ec6e8f65cf325ab822274db311ce22dd3dfce3c
isaacdizolele-open <isaac.dizolele@open-co.com>
2026-04-15 18:10:01 -0300

    Remove campo de data do modelo de Avaliacao e ajuste na exportação de avaliações

--- pages/api/exportar.js
+++ pages/api/exportar.js
@@ -10,7 +10,6 @@ export default async function handler(req, res) {
 
     const linhas = [];
     for (const doc of docs) {
-      const data = new Date(doc.data).toLocaleDateString('pt-BR');
       for (const p of doc.avaliacoes) {
         linhas.push({
           Avaliador: doc.avaliador,
@@ -19,7 +18,6 @@ export default async function handler(req, res) {
           'Comunicação': p.comunicacao,
           'Trabalho em Equipe': p.trabalhoEquipe,
           'Organização': p.organizacao,
-          'Data': data,
         });
       }
     }
@@ -28,7 +26,7 @@ export default async function handler(req, res) {
     const ws = XLSX.utils.json_to_sheet(linhas);
     ws['!cols'] = [
       { wch: 20 }, { wch: 8 }, { wch: 38 },
-      { wch: 14 }, { wch: 18 }, { wch: 12 }, { wch: 12 },
+      { wch: 14 }, { wch: 18 }, { wch: 12 },
     ];
     XLSX.utils.book_append_sheet(wb, ws, 'Avaliações');
 
